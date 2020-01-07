2a31243255b11a36dbb81e45993db510c4d9f361
Jan Borsodi <jb@aplia.no>
2020-01-07 08:21:16 +0000

    Improved redirect operator to support more url types
    A new parameter `type` can be used to control how the resulting
    URL is generated.
    
    It can contain one of
    - abs - Generates an absolute url, with path-prefix/siteaccess
    - root - Generates a relative url without path-prefix/siteaccess
    - absroot - Generates an absolute url, without path-prefix/siteaccess
    
    Additionally improvements:
    - Url starting with double slashes, ie. url without a scheme are supported.
    - Improved https detection, now works if behind a proxy.

--- autoloads/classes/SwarkRedirectOperator.php
+++ autoloads/classes/SwarkRedirectOperator.php
@@ -1,7 +1,7 @@
 <?php
 //
 // Swark - extension for eZ Publish
-// Author: Jan Kudlicka <jk@seeds.no>
+// Author: Jan Kudlicka <jk@seeds.no>, Jan Borsodi <jb@aplia.no>
 // Copyright (C) 2008 Seeds Consulting AS, http://www.seeds.no/
 //
 // This program is free software; you can redistribute it and/or
@@ -23,7 +23,7 @@ class SwarkRedirectOperator extends SwarkOperator
 {
     function __construct()
     {
-        parent::__construct( 'redirect', 'url', 'status=302' );
+        parent::__construct( 'redirect', 'url', 'status=302', 'type=' );
     }
 
     static function execute( $operatorValue, $namedParameters )
@@ -34,20 +34,45 @@ class SwarkRedirectOperator extends SwarkOperator
             $redirectUri = $operatorValue;
         }
 
-        // if $redirectUri is not starting with scheme://
+        $type = $namedParameters['type'];
+        if ( $type && !in_array( $type, array( 'abs', 'root', 'absroot' ) ) )
+        {
+            throw new \Exception("Invalid value for parameter `type`: " . var_export( $type, true ) );
+        }
+
+        // if $redirectUri does not start with scheme://
         if ( !preg_match( '#^\w+://#', $redirectUri ) )
         {
-            // path to eZ Publish index
-            $indexDir = eZSys::indexDir();
+            $absNoScheme = preg_match( '#^//#', $redirectUri );
+            if ( !$absNoScheme && $type !== 'root' && $type !== 'absroot' )
+            {
+                // path to eZ Publish index, includes path-prefix and siteaccess
+                $indexDir = eZSys::indexDir();
 
-            /* We need to make sure we have one
-               and only one slash at the concatenation point
-               between $indexDir and $redirectUri. */
-            $redirectUri = rtrim( $indexDir, '/' ) . '/' . ltrim( $redirectUri, '/' );
+                // We need to make sure we have one
+                // and only one slash at the concatenation point
+                // between $indexDir and $redirectUri.
+                $redirectUri = rtrim( $indexDir, '/' ) . '/' . ltrim( $redirectUri, '/' );
+            }
+
+            // If the `type` parameter is to a 'abs' value then the url is changed to include scheme and hostname
+            // If the url starts with // then it adds the scheme no matter what `type` is set to
+            if ( $absNoScheme )
+            {
+                $schema = eZSys::isSSLNow() ? 'https' : 'http';
+                $redirectUri = "$schema:$redirectUri";
+            }
+            else if ( $type === 'abs' || $type === 'absroot' )
+            {
+                $schema = eZSys::isSSLNow() ? 'https' : 'http';
+                $host = strlen( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
+                $redirectUri = "$schema://$host$redirectUri";
+            }
         }
 
         // Redirect to $redirectUri by returning given status code and exit.
-        eZHTTPTool::redirect( $redirectUri, array(), $namedParameters['status'] );
+        $status = $namedParameters['status'] ? $namedParameters['status'] : 302;
+        eZHTTPTool::redirect( $redirectUri, array(), $status );
         eZExecution::cleanExit();
     }
 }
--- doc/operators/redirect.rst
+++ doc/operators/redirect.rst
@@ -3,13 +3,17 @@ redirect
 
 Summary
 ~~~~~~~
-Stops execution and redirects to the given URL.
+Stops execution and redirects to the given URL. The URL can be specified
+as a relative path in which the siteaccess and path-prefix is prefixed, or
+as an absolute URL. In addition the url format can be tweaked using
+parameter `type`. Redirection is done using the HTTP status code 302
+(temporary redirect) by default but can be changed using parameter `status`.
 
 Usage
 ~~~~~
 ::
 
-    redirect( url [, status] )
+    redirect( url [, status[, type]] )
 
 or
 
@@ -24,6 +28,7 @@ Parameters
     =========== =============================================================== ======== =======
     url         Address to redirect to                                          Yes
     status      HTTP status code                                                No       302
+    type        Control generated url                                           No       
     =========== =============================================================== ======== =======
 
 Examples
@@ -32,16 +37,54 @@ Examples
 
     {redirect( 'book/article' )}
 
-Stops execution and redirects to the node represented by the (eZ Publish) path string book/article.
+Stops execution and redirects to the relative path. The current eZ publish
+siteaccess or other path-prefix is prefixed to the path. e.g. if there is no
+prefix or siteaccess it redirects to `/book/article`, alternatively if the
+siteaccess is `/en` it redirects to `/en/book/article`.
 
 ::
 
     {'http://www.seeds.no'|redirect}
 
-Stops execution and redirects to http://www.seeds.no.
+Stops execution and redirects to absolute URL http://www.seeds.no.
 
 ::
 
     {redirect( $node.parent.url, 301 )}
 
-Stops execution and redirects to the parent node of $node, returning status code 301 (permanent redirection).
+Stops execution and redirects to the parent node of $node, returning status
+code 301 (permanent redirection).
+
+::
+
+    {redirect( $node.parent.url, , 'abs')}
+
+Stops execution and redirects to the parent node but force using an absolute
+url. This will detect http vs https, even behind a proxy, and add the proper
+scheme in front of the host.
+
+::
+
+    {redirect( '/about', , 'root')}
+
+Stops execution and redirects using a relative url but without prefixing the
+eZ publish siteaccess to the path. e.g. if the current siteaccess is '/en'
+the resulting url will be `/about`.
+
+::
+
+    {redirect( '/about', , 'absroot')}
+
+A combination of type `root` and `abs`, ie. it creates an absolute url from
+the relative url but without prefixing the path element with the current
+siteaccess. If the siteaccess is `/en` and the url for the current page is
+`https://example.org/en/content/view` then the resulting url is
+`https://example.org/about`.
+
+::
+
+    {redirect( '//example.org' )}
+
+Redirects to another host and path but using the same scheme as the current
+site. e.g. if the current url is `https://somwhere.example.org/about` will
+redirect to `https://example.org`
